709af4ee4f189ec50e103e264492aaaf53863e0b
wayne <123456>
2017-05-17 11:37:44 +0800

    删掉一行

--- Demo1/src/test/Demo.java
+++ Demo1/src/test/Demo.java
@@ -7,6 +7,5 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("我是A");
         System.out.println("ksajdhas");
-        System.out.println("aaaaaaa");
     }
 }
