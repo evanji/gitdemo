06a571ce312f1f7ec6fe71149b9b24e2c2cfa0cc
Arvin <arvin_hong@gomro.cn>
2017-05-17 10:51:53 +0800

    hh

--- Demo1/src/test/Demo.java
+++ Demo1/src/test/Demo.java
@@ -5,6 +5,7 @@ package test;
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("我是A");
+        System.out.println("我是B");
+        System.out.println("ssssss");
     }
 }
