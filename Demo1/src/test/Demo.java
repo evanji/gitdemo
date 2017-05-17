d7e489e4d24675fb42d07c66f430d9499be47451
wayne <123456>
2017-05-17 14:10:35 +0800

    555666

--- Demo1/src/test/Demo.java
+++ Demo1/src/test/Demo.java
@@ -8,11 +8,16 @@ public class Demo {
         System.out.println("我是ccc1546");
         System.out.println("ksajdhas");
         System.out.println("aaaa");
-        System.out.println("jhj");
+        System.out.println("季红杰");
         System.out.println("个更好换个");
 //        f4cb69bdb9f6a29e622c3a983e76b629fcd55c2e
-        System.out.println(1);
-        System.out.println("2");
+        System.out.println("2424254645");
+        System.out.println(123);
         System.out.println();
+        for(int i=0;i<10;i++)
+        {
+
+
+        }
     }
 }
