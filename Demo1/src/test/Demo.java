9c30b2f69d232c1ecb6d1e2b84595717422d8a80
Arvin <arvin_hong@gomro.cn>
2017-05-17 14:10:50 +0800

    郭中雷

--- Demo1/src/test/Demo.java
+++ Demo1/src/test/Demo.java
@@ -8,11 +8,9 @@ public class Demo {
         System.out.println("我是ccc1546");
         System.out.println("ksajdhas");
         System.out.println("aaaa");
-        System.out.println("jhj");
-        System.out.println("个更好换个");
-//        f4cb69bdb9f6a29e622c3a983e76b629fcd55c2e
-        System.out.println(1);
-        System.out.println("2");
+        System.out.println("gzl");
+        System.out.println("thor");
+
         System.out.println();
     }
 }
