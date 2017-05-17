ba2b48e118c976e0a82713ea32e6b6049d2f44b8
wayne <123456>
2017-05-17 11:44:03 +0800

    一行

--- Demo1/src/test/Demo.java
+++ Demo1/src/test/Demo.java
@@ -5,7 +5,7 @@ package test;
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("我是b");
+        System.out.println("我是a");
         System.out.println("ksajdhas");
         System.out.println("sss");
         System.out.println();
